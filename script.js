7adf52617891fde4e018527cc00734d040fdab89
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-01-04 21:12:29 -0300

    atualização no "if"

--- script.js
+++ script.js
@@ -21,23 +21,23 @@ function displayUserMessage(message) {
 function displayBotMessage(message) {
   const botDiv = document.createElement('div');
   botDiv.className = 'message bot';
-  botDiv.innerHTML = `<strong>Chatbot:</strong> ${message}`;
+  botDiv.innerHTML = `<strong>Assistente:</strong> ${message}`;
   chatBox.appendChild(botDiv);
   chatBox.scrollTop = chatBox.scrollHeight;
 }
 
 function generateResponse(question) {
   let answer;
-  if (question.toLowerCase().includes('cachorro')) {
-    answer = 'Cachorros são animais de estimação adoráveis e leais!';
-  } else if (question.toLowerCase().includes('gato')) {
-    answer = 'Gatos são animais independentes e adoram dormir!';
+  if (question.toLowerCase().includes('amor')) {
+    answer = 'João 3:16 - "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna."';
+  } else if (question.toLowerCase().includes('fé')) {
+    answer = 'Hebreus 11:1 - "Ora, a fé é o firme fundamento das coisas que se esperam, e a prova das coisas que se não veem."';
+
+  } if (question.toLowerCase().includes('deus')) {
+    answer = "Historicamente, Jesus Cristo foi um profeta judeu que viveu na Palestina no século I d.C. Durante sua vida, ele trouxe uma mensagem de libertação a Israel, prometendo a formação de um reino de Deus na Terra. Essa mensagem teria o levado a ser crucificado pelos romanos, os dominadores da Palestina na época.";
+
   } else {
-    answer = 'Desculpe, não sei muito sobre isso. Pergunte sobre gatos ou cachorros!';
-    if (question.toLowerCase().includes('Jesus')) {
-      answer = 'Historicamente, Jesus Cristo foi um profeta judeu que viveu na Palestina no século I d.C. Durante sua vida, ele trouxe uma mensagem de libertação a Israel, prometendo a formação de um reino de Deus na Terra. Essa mensagem teria o levado a ser crucificado pelos romanos, os dominadores da Palestina na época.';
-    }
+    answer = 'Desculpe, não encontrei um versículo específico para essa pergunta. Posso te ajudar com algo mais?';
   }
-  
   displayBotMessage(answer);
 }
