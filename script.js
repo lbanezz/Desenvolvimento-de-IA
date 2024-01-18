318d2ddc886ef3a70c467aff9a833f02a42181af
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-01-18 19:46:54 -0300

    Correção
    foi corrigido as perguntas e realizado alteração na borda do chat botão.

--- script.js
+++ script.js
@@ -26,15 +26,26 @@ function displayBotMessage(message) {
   chatBox.scrollTop = chatBox.scrollHeight;
 }
 
+// perguntas e respostas da inteligencia artificial
+
+
 function generateResponse(question) {
   let answer;
   if (question.toLowerCase().includes('amor')) {
     answer = 'João 3:16 - "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna."';
-  } else if (question.toLowerCase().includes('fé')) {
-    answer = 'Hebreus 11:1 - "Ora, a fé é o firme fundamento das coisas que se esperam, e a prova das coisas que se não veem."';
 
-  } if (question.toLowerCase().includes('deus')) {
-    answer = "Historicamente, Jesus Cristo foi um profeta judeu que viveu na Palestina no século I d.C. Durante sua vida, ele trouxe uma mensagem de libertação a Israel, prometendo a formação de um reino de Deus na Terra. Essa mensagem teria o levado a ser crucificado pelos romanos, os dominadores da Palestina na época.";
+  } else if (question.toLowerCase().includes('fé')) {
+    answer = 'Hebreus 11:1 - "Ora, a fé é o firme fundamento das coisas que se esperam, e a prova das coisas que se não vem.';
+
+  }  else if (question.toLowerCase().includes('deus')) {
+    answer = 'Historicamente, Jesus Cristo foi um profeta judeu que viveu na Palestina no século I d.C. Durante sua vida, ele trouxe uma mensagem de libertação a Israel, prometendo a formação de um reino de Deus na Terra. Essa mensagem teria o levado a ser crucificado pelos romanos, os dominadores da Palestina na época.';
+
+  } else if (question.toLowerCase().includes('estudo bibiclo')) {
+    answer = 'se você deseja um estudo bibiclo via WhatsApp entre em contato com o número +55 12 98200-0062 desejo a você um bom estudo! ';
+
+  } else if (question.toLowerCase().includes('livros que voce recomenda')) {
+    answer = 'se você deseja um estudo bibiclo via WhatsApp entre em contato com o número +55 12 98200-0062 desejo a você um bom estudo! ';
+
 
   } else {
     answer = 'Desculpe, não encontrei um versículo específico para essa pergunta. Posso te ajudar com algo mais?';
